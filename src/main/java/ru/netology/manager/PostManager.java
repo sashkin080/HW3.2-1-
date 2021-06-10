46696a0f5b7b066f80be0af449fba9b4dab8f33b
Alexander Khramov <sssahahram51@gmail.com>
2021-06-10 17:59:18 +0300

    add package manager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.WallVk;
+
+public class PostManager {
+    private WallVk[] wallVk;
+
+    public WallVk[] search(int ownerId, int domain, int query, boolean ownersOnly, int count,int offset){
+        return null;
+    }
+
+    public void delete(int ownerId, int postId) {
+
+    }
+}
